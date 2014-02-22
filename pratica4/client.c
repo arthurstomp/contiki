b37c363c50afe2cad01df0349ad6ff38fce8c912
stomp <arthurstomp@gmail.com>
2014-02-22 23:38:23 +0100

    pratica 4 pronta.

--- pratica4/client.c
+++ pratica4/client.c
@@ -1,26 +1,23 @@
 #include "contiki-net.h"
 #include <stdio.h>
+#include <dev/button-sensor.h>
 
 static struct psock ps;
 static int *buffer;
+static int count = -1;
 
 PROCESS(example_psock_client_process, "Example protosocket client");
 
 static int handle_connection(struct psock *p)
 {
   PSOCK_BEGIN(p);
+  printf("[Client] - handle connection\n");
 
-  //This line is for trigger the server, generate a tcpip_event
-  PSOCK_SEND_STR(p, "hello server\n");
+  int *n;
+  count ++;
+  n = &count;
 
-  while(1){
-    //PSOCK_SEND(p, "test\n", 5);
-    PSOCK_WAIT_UNTIL(p,PSOCK_NEWDATA(p));
-    if(PSOCK_NEWDATA(p)){
-      PSOCK_READBUF_LEN(p,PSOCK_DATALEN(p) );
-      printf("[Cliente] - Got: %i", *buffer);
-    }
-  }
+  PSOCK_SEND(p,n,sizeof(n));
 
   PSOCK_END(p);
 }
@@ -30,26 +27,27 @@ PROCESS_THREAD(example_psock_client_process, ev, data)
   uip_ipaddr_t addr;
 
   PROCESS_BEGIN();
+  SENSORS_ACTIVATE(button_sensor);
 
-  uip_ipaddr(&addr, 172,16,2,0);
-  tcp_connect(&addr, UIP_HTONS(5555), NULL);
+  while(1){
+    PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
+    uip_ipaddr(&addr, 172,16,2,0);
+    tcp_connect(&addr, UIP_HTONS(5555), NULL);
 
-  printf("Connecting...\n");
-  PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event);
+    printf("Connecting...\n");
+    PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event);
 
-  if(uip_aborted() || uip_timedout() || uip_closed()) {
-    printf("Could not establish connection...\n");
-  } else if(uip_connected()) {
-    printf("Connected\n");
+    if(uip_aborted() || uip_timedout() || uip_closed()) {
+      printf("Could not establish connection...\n");
+    } else if(uip_connected()) {
+      printf("Connected\n");
 
-    PSOCK_INIT(&ps, buffer, sizeof(buffer));
+      PSOCK_INIT(&ps, buffer, sizeof(buffer));
 
-    do {
-      handle_connection(&ps);
       PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event);
-    } while(!(uip_closed() || uip_aborted() || uip_timedout()));
-
-    printf("\nConnection closed.\n");
+      handle_connection(&ps);
+      printf("\nConnection closed.\n");
+    }
   }
   PROCESS_END();
 }
--- pratica4/server.c
+++ pratica4/server.c
@@ -1,36 +1,38 @@
 #include <string.h>
 #include <stdlib.h>
 #include "contiki-net.h"
+#include <dev/leds.h> 
 static struct psock ps;
-static char buffer[10];
+static int *buffer;
 
-char * respondWithUpper(struct psock *p){
-  char *newstr = malloc(PSOCK_DATALEN(p));
-  char *stringBuffer = *(char*)buffer;
-  int i = 0;
-  for(i = 0; i < PSOCK_DATALEN(p) ; i++){
-    if(buffer[i] != '\n'){
-      newstr[i] = toupper(buffer[i]);
-    }else{
-      newstr[i] = toupper(buffer[i]);
-    }
+void lightUpLed(int c){
+  if(c % 8 < 4){
+    leds_off(LEDS_BLUE);
+  }else{
+    leds_on(LEDS_BLUE);
   }
-  return newstr;
+  if(c % 4 < 2){
+    leds_off(LEDS_GREEN);
+  }else{
+    leds_on(LEDS_GREEN);
+  }
+  if(c % 2 == 0){
+    leds_off(LEDS_RED);
+  }else{
+    leds_on(LEDS_RED);
+  }
 }
 
 static PT_THREAD(handle_connection(struct psock *p))
 {
   PSOCK_BEGIN(p);
-  int *n;
-  int m = 1;
-  n = &m;
-  //PSOCK_SEND_STR(p, "hello client\n");
-  PSOCK_SEND(p,n,sizeof(n));
-  //PSOCK_READTO(p, '\n');
-  //char *newstr = respondWithUpper(p);
-  //PSOCK_SEND(p, newstr, PSOCK_DATALEN(p));
-  //PSOCK_SEND(p,"Good bye!\r\n",11);
-  //PSOCK_SEND_STR(p, "Good bye!\r\n");
+  printf("[Server] - handle connection\n");
+    PSOCK_WAIT_UNTIL(p,PSOCK_NEWDATA(p));
+    if(PSOCK_NEWDATA(p)){
+      PSOCK_READBUF_LEN(p,PSOCK_DATALEN(p) );
+      printf("[Server] - Got: %i\n", *buffer);
+      lightUpLed(*buffer);
+    }
   PSOCK_CLOSE(p);
   PSOCK_END(p);
 }
@@ -39,6 +41,9 @@ AUTOSTART_PROCESSES(&example_psock_server_process);
 PROCESS_THREAD(example_psock_server_process, ev, data)
 {
   PROCESS_BEGIN();
+  leds_off(LEDS_BLUE);
+  leds_off(LEDS_GREEN);
+  leds_off(LEDS_RED);
   tcp_listen(UIP_HTONS(5555));
   while(1) {
     PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event);
